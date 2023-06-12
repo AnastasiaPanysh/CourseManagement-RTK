0f6ede89c1941746318a7c378e0ed8858fc33a50
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-06-12 23:11:29 +0300

    added client & server

--- client/src/components/Content/Item.js
+++ client/src/components/Content/Item.js
@@ -1,10 +1,13 @@
 import style from './style.module.css'
+import React from 'react'
+import { useGetCourseQuery} from '../../services/course'
 
 function Item() {
+    const { data: dataAll } = useGetCourseQuery()
     return (
         <div className={style.wrapper}>
             <div className={style.block}>
-                <h2>C# Couse</h2>
+            {dataAll ? <h2>{JSON.stringify(dataAll)}</h2> : null}
                 <p>В течение курса студенты ознакомятся с ...</p>
                 <div className={style.flex}>
                     <div className={style.icon}></div>
